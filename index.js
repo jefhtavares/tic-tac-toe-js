e0f2181e9fee7c0d0b59bd17c17e5b3acaa30519
Jéferson Bueno <Jferson Bueno>
2017-03-01 16:25:22 -0300

    construir tabuleiro usando array de array

--- index.js
+++ index.js
@@ -6,14 +6,24 @@
     }
 })(document);
 
-var matriz = [[]];
+var matriz = [];
 var jogadorAtual = 0;
 
-function ready(){    
+function ready(){
     criarTabuleiro(document.getElementById('tabuleiro'));
 }
 
+function initArray(){
+    matriz = new Array(3);
+
+    for(var i = 0; i < 3; i++){
+        matriz[i] = new Array(3);
+    }
+}
+
 function criarTabuleiro(divTabuleiro){
+    initArray();
+
     var templateHtml = document.getElementById('tpl-casa').innerHTML;
 
     var linha = 0, coluna = 0;
@@ -22,11 +32,7 @@ function criarTabuleiro(divTabuleiro){
 
         matriz[linha][coluna] = divTabuleiro.lastElementChild;
 
-        matriz[linha][coluna].addEventListener('click', function(){
-            var casa = this;
-            casa.className += ' marcado ' + (jogadorAtual == 0 ? 'x' : 'o');
-            jogadorAtual = !jogadorAtual;
-        });
+        matriz[linha][coluna].addEventListener('click', marcarCasa);
 
         if((coluna + 1) % 3 === 0){
             linha++;
@@ -37,6 +43,15 @@ function criarTabuleiro(divTabuleiro){
     }
 }
 
-function verificaVencedor(linha1, linha2, linha3){
-    
+function marcarCasa(){
+    var casa = this;
+    var casaMarcada = casa.getAttribute('data-marcado');
+
+    if(casaMarcada == 'true'){
+        return;
+    }
+
+    casa.className += ' marcado ' + (jogadorAtual == 0 ? 'x' : 'o');
+    casa.setAttribute('data-marcado', true);
+    jogadorAtual = !jogadorAtual;
 }
