0914e68b8bbded0900ed0f542b68880e22a05916
Rodrigo Moré <rodrigo.more@planmkt.com.br>
2017-03-22 09:42:39 -0300

    esmaecer tabuleiro ao fim

--- index.js
+++ index.js
@@ -9,8 +9,6 @@
 var tabuleiro = undefined;
 var jogadorAtual = 1;
 
-var somasTabuleiro = { linhas: [0, 0, 0], colunas: [0, 0, 0], diagonais: [0, 0] };
-
 function ready(){
     initArray();
     criarTabuleiro(document.getElementById('tabuleiro'));
@@ -34,7 +32,7 @@ function criarTabuleiro(divTabuleiro){
             divTabuleiro.insertAdjacentHTML('beforeend', htmlCasa);
 
             divTabuleiro.lastElementChild.addEventListener('click', marcarCasa);
-            tabuleiro[linha][coluna] = htmlCasa;
+            tabuleiro[linha][coluna] = 0;
         }
     }
 }
@@ -52,18 +50,8 @@ function marcarCasa(){
     var linha = Number(casa.getAttribute('data-linha'));
     var coluna = Number(casa.getAttribute('data-coluna'));
 
-    //tabuleiro[linha][coluna] = jogadorAtual === 1 ? 3 : 5;
-
-    var s = (jogadorAtual === 1) ? 3 : 5;
-
-    somasTabuleiro.linhas[linha] += s;
-    somasTabuleiro.colunas[coluna] += s;
-
-    if(linha === coluna)
-        somasTabuleiro.diagonais[0] += s;
-    
-    if(linha + coluna === (tabuleiro.length - 1))
-        somasTabuleiro.diagonais[1] += s;
+    var valorJogador = (jogadorAtual === 1) ? 3 : 5;    
+    tabuleiro[linha][coluna] = valorJogador;
 
     verificaVencedor();
     toggleJogador();
@@ -80,29 +68,63 @@ function mostrarJogadorAtual(){
 }
 
 function verificaVencedor(){
-    for(var linha in somasTabuleiro.linhas){
-        if(linha === 9 || linha === 15){
-            marcarVencedores(tabuleiro[0]);
+     var somasLinhas = [0, 0, 0];
+     var somasColunas = [0, 0, 0];
+     var somasDiagonais = [0, 0];
+
+    for(var l = 0; l < tabuleiro.length; l++){        
+        for(var c = 0; c < tabuleiro[l].length; c++){
+            somasLinhas[l] += tabuleiro[l][c];
+            somasColunas[c] += tabuleiro[l][c];
+ 
+            if(l === c)
+               somasDiagonais[0] += tabuleiro[l][c];
+            
+            if(l + c === 2)
+                somasDiagonais[1] += tabuleiro[l][c];
         }
     }
 
-    for(var coluna in somasTabuleiro.colunas){
-        if(linha === 9 || linha === 15){
-            // Alguém venceu
+    for(var i = 0; i < somasLinhas.length; i++){
+        if(somasLinhas[i] === 9 || somasLinhas[i] === 15){
+            marcarVencedores(linha = i);
         }
-    }
 
-    for(var diag in somasTabuleiro.diagonais){
-        if(linha === 9 || linha === 15){
-            // Alguém venceu
+        if(somasColunas[i] === 9 || somasColunas[i] === 15){
+            marcarVencedores(undefined, i);
+        }
+
+        if(i != 3 && (somasDiagonais[i] === 9 || somasDiagonais[i] === 15)){
+            marcarVencedores(undefined, undefined, i);
         }
     }
 }
 
-function marcarVencedores(elementos){
+function marcarVencedores(linha = undefined, coluna = undefined, diagonal = undefined){
     document.getElementById('tabuleiro').className += ' fim';
+    
+    var childDivs = document.getElementById('tabuleiro').getElementsByTagName('div');
+    for(var i = 0; i < childDivs.length; i++ ) {        
+        var casa = childDivs[i];
+        var linhaCasa = casa.getAttribute('data-linha');
+        var colunaCasa = casa.getAttribute('data-coluna');
 
-    for(var i = 0; i < 3; i++){
-        elementos[i].className += ' ganhou';
+        if(linha === linhaCasa || coluna === colunaCasa){
+            casa.setAttribute('class', 'ganhou');
+        }
     }
+}
+
+function logTabuleiro(){
+    var str = '';
+
+    for(var l = 0; l < 3; l++){
+        for(var c = 0; c < 3; c++){
+            str += tabuleiro[l][c] + '   ';
+        }
+
+        str += '\n';
+    }
+
+    console.log(str);
 }
